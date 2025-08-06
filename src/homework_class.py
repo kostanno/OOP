7dff2bb9bdf2b5e0c8dcdc92eaddf6260784fcc2
Kostya Igonin <Igoninqw@gmail.com>
2025-08-06 21:36:30 +0500

    refresh

--- src/homework_class.py
+++ src/homework_class.py
@@ -45,7 +45,7 @@ class Category:
         Category.product_count += len(products)
 
     def add_product(self, product: Product):
-        if issubclass(Category, Product):
+        if isinstance(product, Product):
             self.__products.append(product)
             Category.product_count += 1
 
