6b4725211311c214105601f011d8755903bf7282
Kostya Igonin <Igoninqw@gmail.com>
2025-09-18 09:58:47 +0500

    done raise

--- src/homework_class.py
+++ src/homework_class.py
@@ -1,4 +1,31 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+
+    def __init__(self, name, price, description):
+        super().__init__()
+        self.name = name
+        self.price = price
+        self.description = description
+
+    @abstractmethod
+    def get_full_info(self):
+        return f"{self.name}" f"{self.price}" f"{self.description}"
+
+    def __str__(self):
+        return f"{self.name} - {self.price} руб."
+
+
+class MixinLog:
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}, {self.name}, {self.description}, {self.price}, {self.quantity}"
+
+
+class Product(MixinLog, BaseProduct):
     name: str
     description: str
     price: float
@@ -9,14 +36,17 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+        super().__init__()
 
     @classmethod
-    def new_product(cls, product_data: dict):
+    def new_product(cls, product_data):
         return cls(
-            name=product_data['name'],
-            description=product_data['description'],
-            price=product_data['price'],
-            quantity=product_data['quantity']
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"],
         )
 
     @property
@@ -38,14 +68,30 @@ class Product:
             raise TypeError("Нельзя складывать товары разных классов")
         return (self.price * self.quantity) + (other.price * other.quantity)
 
+    def get_full_info(self):
+        return f"{self.name}" f"{self.price}" f"{self.description}" f"{self.quantity}"
+
 
 class Smartphone(Product):
+
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
+
+    def get_full_info(self):
+        return (
+            f"{self.name}"
+            f"{self.price}"
+            f"{self.description}"
+            f"{self.quantity}"
+            f"{self.efficiency}"
+            f"{self.model}"
+            f"{self.memory}"
+            f"{self.color}"
+        )
 
 
 class LawnGrass(Product):
@@ -54,6 +100,17 @@ class LawnGrass(Product):
         self.country = country
         self.germination_period = germination_period
         self.color = color
+
+    def get_full_info(self):
+        return (
+            f"{self.name}"
+            f"{self.price}"
+            f"{self.description}"
+            f"{self.quantity}"
+            f"{self.germination_period}"
+            f"{self.country}"
+            f"{self.color}"
+        )
 
 
 class Category:
@@ -82,3 +139,12 @@ class Category:
     def __str__(self):
         total_quantity = sum(product.quantity for product in self.__products)
         return f"{self.name}, количество продуктов: {total_quantity} шт."
+
+    def middle_price(self):
+        try:
+            total_price = sum(product.price for product in self.__products)
+            price = total_price / len(self.__products)
+            return price
+        except ZeroDivisionError:
+            print(f"В категории '{self.name}' нет товаров")
+            return 0
